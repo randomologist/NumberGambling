57074185585a2419435fc60dbeb8efc3b4f567fb
randomologist <randomologist2000@gmail.com>
2026-08-22 21:39:13 -0500

    updates to logging

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-let market_val = 500;
-let rent_val = 100;
+function red(num){
+  const out = document.createElement('span');
+  out.innerText="$"+num;
+  out.style.color = "red";
+  return out;
+}
+function green(num){
+  const out = document.createElement('span');
+  out.innerText="$"+num;
+  out.style.color = "green";
+  return out;
+}
+let marketVal = 500;
+let rentVal = 100;
 let bank = 300;
-let bank_num = document.getElementById("bank-value");
-bank_num.innerText = bank;
-let rent_num = document.getElementById("rent-amount");
-rent_num.innerText = rent_val;
-let market_num = document.getElementById("market-value");
-market_num.innerText = "$"+market_val;
-let attacher = document.getElementById("attach-btn");
-let att_state = false
+const bankNum = document.getElementById("bank-value");
+bankNum.innerText = bank;
+const rentNum = document.getElementById("rent-amount");
+rentNum.innerText = rentVal;
+const marketNum = document.getElementById("market-value");
+marketNum.innerText = "$"+marketVal;
+const logs = document.getElementById("logs");
+function dispLog(...elems){
+  const entry =  document.createElement("div")
+  entry.append(...elems);
+  logs.appendChild(entry);
+  logs.scrollTop = logs.scrollHeight;
+  return entry;
+}
+dispLog("Day 0, Bank:$", bank," Market: $", marketVal);
+const attacher = document.getElementById("attach-btn");
+let attState = false
 attacher.addEventListener('click',function(){
-  att_state = !att_state;
-  if(att_state == true){
+  attState = !attState;
+  if(attState == true){
     attacher.innerText="Detach";
   }
   else{
@@ -20,105 +41,112 @@ attacher.addEventListener('click',function(){
 })
 let logged = false;
 let loaner = document.getElementById("loan-btn");
-let loan_box = document.getElementById("loan-input");
-let loan_state = false;
-let loan_val = NaN;
-let init_loan = NaN;
+let loanBox = document.getElementById("loan-input");
+let loanState = false;
+let loanVal = NaN;
+let initLoan = NaN;
 loaner.addEventListener('click',function(){
-  //console.log("init" + loan_val);
-  if(loan_state == false){ //was not borrowing and now is
-    loan_val = loan_box.valueAsNumber;
-    init_loan = loan_val;
-    //console.log("loanchange" + loan_val);
-    if(isNaN(loan_val)){
+  //console.log("init" + loanVal);
+  if(loanState == false){ //was not borrowing and now is
+    loanVal = loanBox.valueAsNumber;
+    initLoan = loanVal;
+    //console.log("loanchange" + loanVal);
+    if(isNaN(loanVal)){
       alert("Fill in the amount you want to borrow");
     }
-    else if(loan_val <=0){
+    else if(loanVal <=0){
       alert("Borrowing amount must be greater than 0")
-      loan_box.select();
+      loanBox.select();
     }
-    else if(loan_val > market_val){
+    else if(loanVal > marketVal){
       alert("Borrowing amount cannot exceed market value")
-      loan_box.select();
+      loanBox.select();
     }
     else{//loan went through
-      loan_state = true;
-      bank += loan_val;
-      loan_box.disabled = true;
+      loanState = true;
+      bank += loanVal;
+      loanBox.disabled = true;
       loaner.innerText="Pay back";
     }
   }
-  else if(loan_state == true){
-    //console.log(loan_state);
-    if(bank < loan_val){
+  else if(loanState == true){
+    //console.log(loanState);
+    if(bank < loanVal){
       alert("You cannot afford to pay back your loan")
     }
     else{//payback went through
-      //console.log("loanval" + loan_val);
-      loan_state = false;
-      loan_box.disabled = false;
-      bank -= loan_val;
+      //console.log("loanval" + loanVal);
+      loanState = false;
+      loanBox.disabled = false;
+      bank -= loanVal;
       loaner.innerText="Borrow";
       if(logged ==true){
-        console.log("Paid back $" + loan_val);
+        dispLog("Paid back ",red(loanVal));
         logged = false;
       }
     }
   }
-  bank_num.innerText=bank;
+  bankNum.innerText=bank;
 })
 let countdown = 10;
-let count_num = document.getElementById("rent-time");
-count_num.innerText = ` in ${countdown} days`;
-let time = document.getElementById("continue-btn");
-let day_num = document.getElementById("day-num");
+const countNum = document.getElementById("rent-time");
+countNum.innerText = ` in ${countdown} days`;
+const time = document.getElementById("continue-btn");
+const dayNum = document.getElementById("day-num");
 let days = 0;
-let change=document.getElementById("market-change");
+const change=document.getElementById("market-change");
 time.addEventListener('click',function(){
   let flux = Math.floor(Math.random()*(-20-21)+21);
-  market_val +=flux;
+  marketVal +=flux;
   change.innerText = " [" + ((flux>0)?"+"+Math.abs(flux):flux) +"]";
-  market_num.innerText = "$"+market_val;
-  flux_color = (flux>0)?"green":"red";
-  market_num.style.color= flux_color;
-  change.style.color = flux_color;
-  if(loan_state==true && logged == false){
-    console.log("Borrowed $" + loan_val);
-    logged=true;
+  marketNum.innerText = "$"+marketVal;
+  fluxColor = (flux>0)?"green":"red";
+  marketNum.style.color= fluxColor;
+  change.style.color = fluxColor;
+  if(loanState==true){
+    if(logged == false){
+      dispLog("Borrowed ",green(loanVal));
+      logged=true;
+    }
+      //payback ceiling
+      console.log(initLoan);
+      loanVal = Math.min(marketVal,initLoan);
+      loanBox.value = loanVal;
+      //console.log(loanBox.value);
+      //console.log("boxchanged?" + loanVal);
   }
-    //payback ceiling
-    loan_val = Math.min(market_val,init_loan);
-    loan_box.value = loan_val;
-    //console.log(loan_box.value);
-    //console.log("boxchanged?" + loan_val);
-  if(att_state ==true){
+  if(attState ==true){
     bank+=flux;
-    bank_num.innerText=bank;
+    bankNum.innerText=bank;
   }
   countdown -=1;
   if(countdown == 1){
-    count_num.innerText="!";
+    countNum.innerText="!";
   }
-  else{count_num.innerText = ` in ${countdown} days`;}
+  else{countNum.innerText = ` in ${countdown} days`;}
   if(countdown<=0){//rent check
-    if(bank - rent_val <0){
+    if(bank - rentVal <0){
       document.getElementById("current").innerHTML = 
         `<main>
           <div>Game Over</div>
           <div>You lasted ${days} days</div>
-          <div>Market: ${market_val}</div>
-          <div>Last rent cost: ${rent_val}</div>
+          <div>Market: ${marketVal}</div>
+          <div>Last rent cost: ${rentVal}</div>
         </main>`;
     }
     else{
-      bank = bank-rent_val;
-      rent_val+=20;
+      bank = bank-rentVal;
+      rentVal+=20;
       countdown = 10;
-      count_num.innerText = ` in ${countdown} days`;
-      bank_num.innerText=bank;
-      rent_num.innerText=rent_val;
+      countNum.innerText = ` in ${countdown} days`;
+      bankNum.innerText=bank;
+      rentNum.innerText=rentVal;
     }
   }
   days +=1;
-  day_num.innerText = days;
+  dayNum.innerText = days;
+  let marketLog = change.cloneNode(true);
+  marketLog.removeAttribute("id");
+  let dayLog = dispLog("Day ",days,", Bank:$", bank," Market: $", marketVal,marketLog);
+  dayLog.style.borderTop="1px dotted";
 })
